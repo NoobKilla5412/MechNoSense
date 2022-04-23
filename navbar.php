ba6d11217c11a0e06a4652978f49237bc62a9d33
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-04-23 16:47:49 -0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -1,7 +1,7 @@
 <?php function active($doc1) {
 	global $doc;
 	if ($doc === $doc1) {
-		echo "active";
+		return "active";
 	}
 }?>
 <nav class="navbar navbar-inverse">
@@ -16,7 +16,7 @@
 					</div>
 					<div class="collapse navbar-collapse" id="links">
 						<ul class="nav navbar-nav">
-							<li class="" ><a href="/">Home</a></li>
+							<li class="<?=active('Home');?>" ><a href="/">Home</a></li>
 							<li class="" ><a href="about-us">About Us</a></li>
 							<li class="" ><a href="workshops">Workshops</a>
 							<li class="" ><a href="daily-log">Daily Log</a>
