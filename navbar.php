43f29cffc315fd8939f2906f6249fe8c430352ac
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-04-23 16:44:28 -0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -16,19 +16,19 @@
 					</div>
 					<div class="collapse navbar-collapse" id="links">
 						<ul class="nav navbar-nav">
-							<li class=<?php active("Home");?>><a href="/">Home</a></li>
-							<li class="<?php active("About Us");?>"><a href="about-us">About Us</a></li>
-							<li class="<?php active("Workshops");?>"><a href="workshops">Workshops</a>
-							<li class="<?php active("Daily Log");?>"><a href="daily-log">Daily Log</a>
-							<li class="<?php active("Sign Up");?>"><a href="signup">Sign Up</a>
-							<li class="<?php active("Videos");?>"><a href="videos">Videos</a></li>
-							<li class="dropdown <?php active("Sponsors");?>">
+							<li class="<?php active("Home");?>" ><a href="/">Home</a></li>
+							<li class="<?php active("About Us");?>" ><a href="about-us">About Us</a></li>
+							<li class="<?php active("Workshops");?>" ><a href="workshops">Workshops</a>
+							<li class="<?php active("Daily Log");?>" ><a href="daily-log">Daily Log</a>
+							<li class="<?php active("Sign Up");?>" ><a href="signup">Sign Up</a>
+							<li class="<?php active("Videos");?>" ><a href="videos">Videos</a></li>
+							<li class="dropdown <?php active("Sponsors");?>" >
 								<a class="dropdown-toggle" data-toggle="dropdown">
 									Sponsors <span class="caret"/>
 								</a>
 								<ul class="dropdown-menu" style="background-color:#183820;">
-									<li class="<?php active("Sponsors");?>"><a href="/sponsors"><b>Sponsors</b></a></li>
-									<li class="<?php active("Sponsors-HP");?>"><a href="/sponsors/hp">HP</a></li>
+									<li class="<?php active("Sponsors");?>" ><a href="/sponsors"><b>Sponsors</b></a></li>
+									<li class="<?php active("Sponsors-HP");?>" ><a href="/sponsors/hp">HP</a></li>
 								</ul>
 							</li>
 							<li class="dropdown">
