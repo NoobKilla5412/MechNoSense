72fa906c81f2edb600b786fb6cd87d1dac73fa25
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-04-23 17:43:17 -0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -6,7 +6,16 @@ function active($doc1) {
 	}else {
 		echo "<li>";
 	}
-}?>
+}
+function activeDrop($doc1) {
+	global $doc;
+	if ($doc === $doc1) {
+		echo "<li class=\"dropdown active\">";
+	}else {
+		echo "<li class=dropdown>";
+	}
+}
+?>
 <nav class="navbar navbar-inverse">
 					<div class="navbar-header">
 						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#links" aria-expanded="false">
@@ -20,38 +29,38 @@ function active($doc1) {
 					<div class="collapse navbar-collapse" id="links">
 						<ul class="nav navbar-nav">
 							<?php echo active('Home');?><a href="/">Home</a></li>
-							<li><a href="about-us">About Us</a></li>
-							<li><a href="workshops">Workshops</a>
-							<li><a href="daily-log">Daily Log</a>
-							<li><a href="signup">Sign Up</a>
-							<li><a href="videos">Videos</a></li>
-							<li class="dropdown" >
+							<?php echo active('About Us');?><a href="about-us">About Us</a></li>
+							<?php echo active('Workshops');?><a href="workshops">Workshops</a>
+							<?php echo active('Daily Log');?><a href="daily-log">Daily Log</a>
+							<?php echo active('Sign Up');?><a href="signup">Sign Up</a>
+							<?php echo active('Videos');?><a href="videos">Videos</a></li>
+							<?php echo activeDrop('Sponsors');?>
 								<a class="dropdown-toggle" data-toggle="dropdown">
 									Sponsors <span class="caret"/>
 								</a>
 								<ul class="dropdown-menu" style="background-color:#183820;">
-									<li class="" ><a href="/sponsors"><b>Sponsors</b></a></li>
-									<li class="" ><a href="/sponsors/hp">HP</a></li>
+									<?php echo active('Sponsors');?><a href="/sponsors"><b>Sponsors</b></a></li>
+									<?php echo active('Sponsors-HP');?><a href="/sponsors/hp">HP</a></li>
 								</ul>
 							</li>
-							<li class="dropdown">
+							<?php echo activeDrop('Robots');?>
 								<a class="dropdown-toggle" data-toggle="dropdown">
 									Robots <span class="caret"/>
 								</a>
 								<ul class="dropdown-menu" style="background-color:#183820;">
-									<li class=""><a href="robots"><b>Robots</b></a></li>
-									<li class=""><a href="robots/titans#titans">Titans</a></li>
-									<li class=""><a href="robots/atlas#atlas">Atlas</a></li>
+									<?php echo active('Robots');?><a href="robots"><b>Robots</b></a></li>
+									<?php echo active('Robots-Titans');?><a href="robots/titans#titans">Titans</a></li>
+									<?php echo active('Robots-Atlas');?><a href="robots/atlas#atlas">Atlas</a></li>
 								</ul>
 							</li>
-							<li class="dropdown">
+							<?php echo activeDrop('Buy');?>
 								<a class="dropdown-toggle" data-toggle="dropdown">
 									Buy <span class="caret"/>
 								</a>
 								<ul class="dropdown-menu" style="background-color:#183820;">
-									<li class=""><a href="buy"><b>Buy</b></a></li>
-									<li class=""><a href="buy#titans">Titans</a></li>
-									<li class=""><a href="buy#atlas">Atlas</a></li>
+									<?php echo active('Buy');?><a href="buy"><b>Buy</b></a></li>
+									<?php echo active('Buy-Titans');?><a href="buy#titans">Titans</a></li>
+									<?php echo active('Buy-Atlas');?><a href="buy#atlas">Atlas</a></li>
 								</ul>
 							</li>
 						</ul>
