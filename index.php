740178336fd00f42644074b3ea6beb6aefebc3a6
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-04-23 17:21:54 -0700

    Update index.php

--- index.php
+++ index.php
@@ -52,7 +52,7 @@ function active($doc1) {
 			<hr />
 			<span class="names">Brandon, Liana, Ismael, Dane, Linnaea, Vibhav, Isaac, Grae, Matthew, Will, Linnaea</span>
 			<hr />
-			<?php echo file_get_contents('.\navbar.php');?>
+			<?php include '.\navbar.php');?>
 		</div>
 			<main>
 				<h3 id="team">
