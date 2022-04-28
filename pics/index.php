0488eccae9e4acfd95f13acd9e864b1994fc7f95
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-04-27 21:17:47 -0700

    Update index.php

--- pics/index.php
+++ pics/index.php
@@ -49,27 +49,27 @@ $doc = "Pictures";
 			<?php include '..\navbar.php';?>
 		</div>
 		<div class="row">
-			<div class="col-md-4">
+			<div class="col-md-7">
 				<img width="750px"src="/pics/images/IMG_6502.png" />
 				<img width="750px"src="/pics/images/IMG_6503.png" />
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-7">
 				<img width="750px"src="/pics/images/IMG_6504.png" />
 				<img width="750px"src="/pics/images/IMG_6505.png" />
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-7">
 				<img width="750px"src="/pics/images/IMG_6506.png" />
 				<img width="750px"src="/pics/images/IMG_6507.png" />
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-7">
 				<img width="750px"src="/pics/images/IMG_6508.png" />
 				<img width="750px"src="/pics/images/IMG_6510.png" />
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-7">
 				<img width="750px"src="/pics/images/IMG_6511.png" />
 				<img width="750px"src="/pics/images/IMG_6512.png" />
 			</div>
-			<div class="col-md-4">
+			<div class="col-md-7">
 				<img width="750px"src="/pics/images/IMG_6513.png" />
 			</div>
 		</div>
