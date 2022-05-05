d6768f95e59511ba17d9bbb25b771dc85c937e0c
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-04 19:44:30 -0700

    Update index.php

--- workshops/index.php
+++ workshops/index.php
@@ -29,7 +29,7 @@ $doc = "Workshops";
 	<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"
 		integrity="sha384-Tc5IQib027qvyjSMfHjOMaLkfuWVxZxUPnCJA7l2mCWNIpG9mGCD8wGNIcPD7Txa" crossorigin="anonymous">
 	</script>
-	<link rel="stylesheet" href="/style.css" />
+	<link rel="stylesheet" href="../style.css" />
 	<!-- Custom CSS -->
 	<title>Workshops &#8211; MechNoSense</title>
 </head>
