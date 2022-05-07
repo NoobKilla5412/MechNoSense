20f9ac14d1ba00edccd011b9c2729ce510dec7dc
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-06 17:08:08 -0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -74,7 +74,7 @@ f="/robots/"><b>Robots</b></a></li>
 					<a class="nav-link disabled">Disabled</a>
 				</li>
 			</ul>
-			<form class="d-flex">
+			<form class="d-flex" action="/search">
 				<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
 				<button class="btn btn-outline-success" type="submit">Search</button>
 			</form>
