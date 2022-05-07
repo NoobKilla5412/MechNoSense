a855e4440d30c015f67afc7ef5ddc4ffee4316f1
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-06 21:28:35 -0700

    Create sitemap.php

---
+++ sitemap.php
@@ -0,0 +1,53 @@
+<?php
+$doc = "Home";
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VC2S111058"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-VC2S111058');
+	</script>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="description" content="MechNoSense Robotics Home Page">
+	<meta name="keywords" content="MechNoSense mechnosense robots robotics robot">
+	<link rel="icon" type="image/ico" href="/favicon.ico" />
+	<link rel="apple-touch-icon" href="/favicon.min.png">
+	<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- Bootstrap JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<!-- Custom CSS -->
+	<link rel="stylesheet" href="/style.min.css" />
+	<title>Sitemap &#8211; MechNoSense</title>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="jumbotron text-center">
+			<!-- Navbar -->
+			<?php include '.\navbar.php';?>
+			<br />
+
+			<h1 id="title123">
+				MechNoSense Sitemap
+			</h1>
+			<hr />
+			<span class="names">Brandon, Liana, Ismael, Dane, Linnaea, Vibhav, Isaac, Grae, Matthew, Will</span>
+			<hr />
+		</div>
+		<a href="http://www.mechnosense.org/">Home</a> <a href="http://mechnosense.org/about-us/">About Us</a> <a href="http://www.mechnosense.org/daily-log/">Daily Log</a> <a href="http://www.mechnosense.org/signup/">Sign Up</a> <a href="http://www.mechnosense.org/videos/">Videos</a> <a href="http://www.mechnosense.org/pics/">Pictures</a> <a href="http://www.mechnosense.org/buy/">Buy</a>
+	</div>
+</body>
+
+</html>
