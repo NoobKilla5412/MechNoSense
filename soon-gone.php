18f5a78837b8a84435346236cb543602e01297d3
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-11 19:41:56 -0700

    Update soon-gone.php

--- soon-gone.php
+++ soon-gone.php
@@ -30,6 +30,9 @@
 <body>
 	<div class="container-fluid">
 		<div class="jumbotron text-center">
+			<script>
+				Leaving.show();
+			</script>
 			<!-- Navbar -->
 			<?php include '.\navbar.php';?>
 			<br />
@@ -56,9 +59,6 @@
 		<p class="text-center">
 			This Page is Leaving. Click this button for the home page<br/><a href="/" class="btn btn-default" style="background-color: #185020;"><span style="color: #A48C38;">Home</span></a>
 		</p>
-		<script>
-			show.bs.Leaving();
-		</script>
 	</div>
 </body>
 
