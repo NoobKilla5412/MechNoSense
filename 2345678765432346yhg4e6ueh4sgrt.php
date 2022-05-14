ce215203cd43aea67c85d6bda42e3c5f5134359a
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-13 19:36:33 -0700

    Create 2345678765432346yhg4e6ueh4sgrt.php

---
+++ 2345678765432346yhg4e6ueh4sgrt.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+	<script async src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-3053586612094756" crossorigin="anonymous"></script>
+	<meta charset="utf-8">
+	<link rel="manifest" href="/manifest.json">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<link rel="icon" type="image/ico" href="/favicon.ico" />
+	<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- Bootstrap JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="/style.min.css" />
+	<!-- Custom CSS -->
+	<title>This is me messing around with adds &#8211; MechNoSense</title>
+</head>
+
+<body>
+	<div class="container-fluid">
+		<div class="jumbotron text-center">
+			<!-- Navbar -->
+			<?php include '.\navbar.php';?>
+			<br />
+			<h1 id="title123">
+				This is me messing around with adds
+			</h1>
+			<hr />
+			<span class="names">Brandon, Liana, Ismael, Dane, Linnaea, Vibhav, Isaac, Grae, Matthew, Will</span>
+			<hr />
+		</div>
+		<p class="text-center">
+			It looks like you are lost. Click this button for the home page<br/><a href="/" class="btn btn-default" style="background-color: #185020;"><span style="color: #A48C38;">Home</span></a>
+		</p>
+	</div>
+</body>
+
+</html>
