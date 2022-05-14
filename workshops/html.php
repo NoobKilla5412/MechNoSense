1f7fe7e5d8814dce3436f9219e9e11a260a37c87
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-13 19:51:22 -0700

    Create html.php

---
+++ workshops/html.php
@@ -0,0 +1,52 @@
+<?php
+$doc = "Workshops-HTML";
+$subdoc = "Workshops";
+?>
+<!DOCTYPE html>
+<html lang="en-us">
+
+<head>
+	<!-- Global site tag (gtag.js) - Google Analytics -->
+	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VC2S111058"></script>
+	<script>
+		window.dataLayer = window.dataLayer || [];
+			function gtag(){dataLayer.push(arguments);}
+			gtag('js', new Date());
+
+			gtag('config', 'G-H9Z681C9TH');
+	</script>
+	<meta charset="utf-8">
+	<link rel="manifest" href="/manifest.json">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<meta name="keywords" content="MechNoSense mechnosense robots robotics robot">
+	<link rel="icon" type="image/ico" href="/favicon.ico" />
+	<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+	<!-- Bootstrap CSS -->
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<!-- Bootstrap JavaScript -->
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<link rel="stylesheet" href="/style.min.css" />
+	<!-- Custom CSS -->
+	<title>HTML &#8211; Workshops | MechNoSense</title>
+</head>
+
+<body>
+<div class="container-fluid">
+	<div class="jumbotron text-center">
+		<!-- Navbar -->
+		<?php include '..\navbar.php';?>
+		<br />
+		<h1 id="title123">
+			Workshops
+		</h1>
+		<hr />
+		<span class="names">Brandon, Liana, Ismael, Dane, Linnaea, Vibhav, Isaac, Grae, Matthew, Will</span>
+		<hr />
+		</div>
+		<a target="_blank" href="https://calendar.google.com/event?action=TEMPLATE&amp;tmeid=NXVkMnFubW41Ymp2MGtnMGVxdmF1Y3J0YWEgbWVjaG5vc2Vuc2VAbQ&amp;tmsrc=mechnosense%40gmail.com"><img border="0" src="https://www.google.com/calendar/images/ext/gc_button1_en.gif"></a>
+	</div>
+</body>
+
+</html>
