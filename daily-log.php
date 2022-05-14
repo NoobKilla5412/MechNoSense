4234b8ffe5811324b6ab94741d6a5b56f2dabda5
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-13 20:29:43 -0700

    Update daily-log.php

--- daily-log.php
+++ daily-log.php
@@ -130,6 +130,7 @@ $doc = "Daily Log";
 				<p>
 					In build today we worked on two drive bases, an x-drive and a v-drive that we decided was a cool idea and we had some extra parts. We drew some designs and decided these were the best ideas. We cleaned up early and finished on time. Ismael pushed the day's code to our git.
 				</p>
+				<a href="#">Back to top</a>
 			</div>
 		</div>
 	</body>
