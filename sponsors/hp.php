c0ba35d4e9df862e80b397e5c0d39a0af6815059
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-05-29 15:05:47 -0700

    Update hp.php

--- sponsors/hp.php
+++ sponsors/hp.php
@@ -4,59 +4,96 @@ $subdoc = "Sponsors";
 ?>
 <!DOCTYPE html>
 <html lang="en-us">
-	<head>
-		<!-- Global site tag (gtag.js) - Google Analytics -->
-		<script async src="https://www.googletagmanager.com/gtag/js?id=G-VC2S111058"></script>
-		<script>
-			window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
 
-			gtag('config', 'G-H9Z681C9TH');
-		</script>
-		<meta charset="utf-8">
-	<link rel="manifest" href="/manifest.json">
-		<meta http-equiv="X-UA-Compatible" content="IE=edge">
-		<meta name="viewport" content="width=device-width, initial-scale=1">
-		<meta name="keywords" content="MechNoSense mechnosense robots robotics robot">
-		<link rel="icon" type="image/ico" href="/favicon.ico" />
-		<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<head>
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VC2S111058"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
 
-		<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<!-- Bootstrap JavaScript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-		<link rel="stylesheet" href="/style.min.css"/>
-		<!-- Custom CSS -->
-		<title>HP &#8211; Sponsors | MechNoSense</title>
-	</head>
-	<body>
-		<div class="container-fluid">
-			<!-- Navbar -->
-			<?php include '..\navbar.php';?>
-			<div class="jumbotron text-center">
-				<h1 id="title123">
-					HP
-				</h1>
-				<hr/>
-				<span class="names">Brandon, Liana, Ismael, Dane, Linnaea, Vibhav, Isaac, Grae, Matthew, Will</span>
-				<hr/>
-			</div>
-			<p>
-				Dear HP Inc., we thank you for your enormous generosity with your donation to our club. We are extremely grateful towards your graciousness. Without your grant we would not have been able to continue our aspirations within robotics. The funds will help us in many ways. Some of the ways that it will help us is that we can order new supplies, which include, two new drive systems, many small parts, and three new computers. We can progress greatly and learn many new things with the funds that you have provided us. Here are some photos from our competition mentioned below, as well as how we did during the competition.
-			</p>
-			<img src="/pics/images/IMG_6502.png" width="50%"/>
-			<p>
-				This is the photo of one of our robots on a subteam within Rocket Robotics which we call the “Titans”. This robot included many features such as a linear slide elevator which took our bucket up to the right level so we could drop off blocks and balls, which were some of the scoring elements in the competition. The bucket was printed using PLA filament. The robot also includes a spinner(red) which has been designed using OnShape software, and uses TPU filament, which makes it ideal for picking up cubes and balls with its bendable properties. Lastly it includes a carousel spinner which are just two gecko wheels from GoBuilda placed on top of each her and powered by a motor to turn the Carousel which is also one of the scoring elements within the competition.
-			</p>
-			<img src="/pics/images/atlas.jpg" width="50%"/>
-			<p>
-				This is a photo of the other robot from the counterpart to the subteam “Titans” called “Atlas”. This robot also includes a linear slide similar to the Titans robot. Rather than using a bucket we used a revolutionary fork design which in fact worked extremely well even though it faced extreme skepticism in the beginning. This allows it to pick up scoring elements such as the wiffle ball which has multiple holes where the fork can fit and also the cube which has holes in the button and top. The small and compact design won it the Most Unique Design Award in our competition. The unique design allows it to be very stealthy and can pass through small spaces very easily. One of the unorthodox techniques that was used during the building of this robot is that we put an extra motor on the linear slide as just a dummy weight to pull the linear slide back down using gravity
-			</p>
-			<img src="/pics/images/team.jpg" width="100%"/>
-			<p>
-				This is a photo from one of the competitions. We performed really well and also found out a lot about the problems within our robot and how to fix them. We learned from our mistakes and even though we weren't able to do many changes to the robot both mechanical and program wise, we just had to go with the flow and try to brainstorm out of the box ideas to try and solve our problems. We enjoyed this competition and that was our main goal. This particular photo shows the Titans robot and the Titans team. From left to right we have, Vibhav , our co-leader and programmer in our team, Ismael, one of our newest joinees to the programming team, Dane, who is our main builder, and finally Linnaea, the other co-leader, businessperson, engineering notebook person, and the programmer. To wrap it up, we sincerely thank you for providing us with the funds necessary to continue this team for the following season.
-			</p>
-		</div>
-	</body>
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
+
+    gtag('config', 'G-H9Z681C9TH');
+    </script>
+    <meta charset="utf-8">
+    <link rel="manifest" href="/manifest.json">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="keywords" content="MechNoSense mechnosense robots robotics robot">
+    <link rel="icon" type="image/ico" href="/favicon.ico" />
+    <script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="/style.min.css" />
+    <!-- Custom CSS -->
+    <title>HP &#8211; Sponsors | MechNoSense</title>
+</head>
+
+<body>
+    <div class="container-fluid">
+        <!-- Navbar -->
+        <?php include '..\navbar.php';?>
+        <div class="jumbotron text-center">
+            <h1 id="title123">
+                HP
+            </h1>
+            <hr />
+            <span class="names">Brandon, Liana, Ismael, Dane, Linnaea, Vibhav, Isaac, Grae, Matthew, Will</span>
+            <hr />
+        </div>
+        <p>
+            Dear HP Inc., we thank you for your enormous generosity with your donation to our club. We are extremely
+            grateful for your graciousness. Without your grant we would not have been able to continue our
+            aspirations within robotics. The funds will help us in many ways. Some of the ways that it will help us is
+            that we can order new supplies, which include two new drive systems, many small parts, and three new
+            computers. We can progress greatly and learn many new things with the funds that you have provided us. Here
+            are some photos from our competition mentioned below, as well as how we did during the competition.
+        </p>
+        <img src="/pics/images/IMG_6502.png" width="50%" />
+        <p>
+            This is the photo of one of our robots on a subteam within Rocket Robotics which we call the “Titans”. This
+            robot included many features such as a linear slide elevator which took our bucket up to the right level so
+            we could drop off blocks and balls, which were some of the scoring elements in the competition. The bucket
+            was printed using PLA filament. The robot also includes a spinner(red) which has been designed using OnShape
+            software, and uses TPU filament, which makes it ideal for picking up cubes and balls with its bendable
+            properties. Lastly it includes a carousel spinner which are just two gecko wheels from GoBuilda placed on
+            top of each her and powered by a motor to turn the Carousel which is also one of the scoring elements within
+            the competition.
+        </p>
+        <img src="/pics/images/atlas.jpg" width="50%" />
+        <p>
+            This is a photo of the other robot from the counterpart to the subteam “Titans” called “Atlas”. This robot
+            also includes a linear slide similar to the Titans robot. Rather than using a bucket we used a revolutionary
+            fork design which in fact worked extremely well even though it faced extreme skepticism in the beginning.
+            This allows it to pick up scoring elements such as the wiffle ball which has multiple holes where the fork
+            can fit and also the cube which has holes in the button and top. The small and compact design won it the
+            Most Unique Design Award in our competition. The unique design allows it to be very stealthy and can pass
+            through small spaces very easily. One of the unorthodox techniques that was used during the building of this
+            robot is that we put an extra motor on the linear slide as just a dummy weight to pull the linear slide back
+            down using gravity
+        </p>
+        <img src="/pics/images/team.jpg" width="100%" />
+        <p>
+            This is a photo from one of the competitions. We performed really well and also found out a lot about the
+            problems within our robot and how to fix them. We learned from our mistakes and even though we weren't able
+            to do many changes to the robot both mechanical and program wise, we just had to go with the flow and try to
+            brainstorm out of the box ideas to try and solve our problems. We enjoyed this competition and that was our
+            main goal. This particular photo shows the Titans robot and the Titans team. From left to right we have,
+            Vibhav , our co-leader and programmer in our team, Ismael, one of our newest joinees to the programming
+            team, Dane, who is our main builder, and finally Linnaea, the other co-leader, businessperson, engineering
+            notebook person, and the programmer. To wrap it up, we sincerely thank you for providing us with the funds
+            necessary to continue this team for the following season.
+        </p>
+    </div>
+</body>
+
 </html>
