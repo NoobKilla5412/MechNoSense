cbfc93a4dee28871a72c902cc9b2e889d7f4f5ef
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-12 10:12:06 -0700

    Reformat code

--- about-us.php
+++ about-us.php
@@ -5,93 +5,128 @@ $doc = "About Us";
 <html lang="en-us">
 
 <head>
-	<!-- Global site tag (gtag.js) - Google Analytics -->
-	<script async src="https://www.googletagmanager.com/gtag/js?id=G-VC2S111058"></script>
-	<script>
-		window.dataLayer = window.dataLayer || [];
-			function gtag(){dataLayer.push(arguments);}
-			gtag('js', new Date());
+    <!-- Global site tag (gtag.js) - Google Analytics -->
+    <script async src="https://www.googletagmanager.com/gtag/js?id=G-VC2S111058"></script>
+    <script>
+    window.dataLayer = window.dataLayer || [];
 
-			gtag('config', 'G-H9Z681C9TH');
-	</script>
-	<meta charset="utf-8">
-	<link rel="manifest" href="/manifest.json">
-	<meta http-equiv="X-UA-Compatible" content="IE=edge">
-	<meta name="viewport" content="width=device-width, initial-scale=1">
-	<link rel="icon" type="image/ico" href="/favicon.ico" />
-	<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+    function gtag() {
+        dataLayer.push(arguments);
+    }
+    gtag('js', new Date());
 
-	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-	<!-- Bootstrap JavaScript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-	<link rel="stylesheet" href="/style.min.css" />
-	<!-- Custom CSS -->
-	<title>About Us &#8211; MechNoSense</title>
+    gtag('config', 'G-H9Z681C9TH');
+    </script>
+    <meta charset="utf-8">
+    <link rel="manifest" href="/manifest.json">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/ico" href="/favicon.ico" />
+    <script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <!-- Bootstrap JavaScript -->
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+        integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous">
+    </script>
+    <link rel="stylesheet" href="/style.min.css" />
+    <!-- Custom CSS -->
+    <title>About Us &#8211; MechNoSense</title>
 </head>
 
 <body>
-	<div class="container-fluid">
-		<!-- Navbar -->
-		<?php include '.\navbar.php';?>
-		<div class="jumbotron text-center">
-			<h1 id="title123">
-				About Our Team
-			</h1>
-			<hr />
-			<span class="names"><a href="#brandon" class="names-link">Brandon</a>, <a href="#liana" class="names-link">Liana</a>, <a href="#ismael" class="names-link">Ismael</a>, <a href="#dane" class="names-link">Dane</a>, <a href="#linnaea" class="names-link">Linnaea</a>, <a href="#vibhav" class="names-link">Vibhav</a>, <a href="#isaac" class="names-link">Isaac</a>, <a href="#grae" class="names-link">Grae</a>, <a href="#matthew" class="names-link">Matthew</a>, <a href="#will" class="names-link">Will</a>, <a href="#linnaea" class="names-link">Linnaea</a> | <a href="mailto:contact@mechnosense.org" class="names-link">Our&nbsp;email</a></span>
-			<hr />
-			</div>
-		<!-- <h2 style="font-size: 200pt;"> -->
-			<!-- This is our about page -->
-		<!-- </h2> -->
-		<h3>Team Biographies:</h3>
-		<h4>Linnaea:</h4>
-		<p id="linnaea">
-			I'm the team lead this year and am excited to start off this year with a blast. I'm part of team rocket robotics and have been part of this for about three years now. I enjoy managing our repo, making plans, and coding. Outside of robotics, I like to Hike, camp, and all things outdoors. I'm a team leader and am strong at long-term planning and short-term emergencies. And I really enjoy giving help whenever needed. I've enjoyed these three years and am stoked to have another amazing year with team 12110 Mechnosense.
-		</p>
-		<h4>Liana:</h4>
-		<p id="liana">
-			I am the creative input for our website, a builder, and I also work on the engineering notebook/note-taking. I joined in 2021 as a builder, and I worked on the robot. This year I want to learn more about building, and maybe learn about coding. The website is my first big project, and I’m very excited about it.
-		</p>
-		<h4>Ismael:</h4>
-		<p id="ismael">
-			I’m Ismael and this is my first year in robotics. I am focusing on coding our robot’s teleop and doing cool stuff to put in there like PID and computer vision. I am really excited to learn how to do all this. I am in seventh grade and am running out of things to say but just to make it seem like mine is the longest I will put this in.
-		</p>
-		<h4>Dane:</h4>
-		<p id="dane">
-			I am Dane. I am our head builder and I joined in 2021. I have loved building things all my life. I was also the lead builder for last year's robotics challenge (freight frenzy). Probably the biggest reason I joined was to pit my hard work against others who also love to build.
-		</p>
-		<h4>Vibhav:</h4>
-		<p id="vibhav">
-			I am Vibhav. The co-team leader, and co-lead programmer. I have been doing Robotics for 2+ years now and have been programming for most of the time as well. I became interested in Robotics partly because of my parents. They are both software engineers and I was very interested in their work. The other part was that I had played around with coding for some time and I found that I enjoyed making small games and programs with it; so that’s how I became interested in robotics and programming specifically. This year, I am very excited to learn more, work harder, and do better!
-		</p>
-		<h4>Isaac:</h4>
-		<p id="isaac">
-			My name is Isaac and this is my second season of robotics in this club. I am one of our group's builders but I have been working in many areas of robotics this season. I have really enjoyed robotics and I want to show its greatness to anyone who wants it. My favorite color is orange, I like robotics and soccer, I'm in middle school and pizza is good. Goodbyeeeee. 
-		</p>
-		<h4>Grae:</h4>
-		<p id="grae">
-			Hi my name is Grae.
-		</p>
-		<h4>Matthew:</h4>
-		<p id="matthew">
-			Hi my name is Matthew.
-		</p>
-		<h4>Will:</h4>
-		<p id="will">
-			My name is Will I., programmer for team mechnosense. I was the leader for one of the two sub-teams last year: team 13749 atlas. I am currently a midline programmer for mechnosense, as well as an assistant coordinator for mechnosense.
-		</p>
-		<h4>Brandon:</h4>
-		<p id="brandon">
-			Hi, my name is Brandon. I do the coding of this webpage. I drive the robot for the competitions.
-		</p>
-		<p id="email">
-			Our email is <a href="mailto:contact@mechnosense.org">contact@mechnosense.org</a>
-		</p>
-		<p id="YouTube">
-			Our YouTube channel is <a href="https://www.youtube.com/channel/UCBLlVrpF1RP2r3pBj7l6z8Q">Mechnosense Robotics</a>
-		</p>
-	</div>
-	</body>
+    <div class="container-fluid">
+        <!-- Navbar -->
+        <?php include '.\navbar.php';?>
+        <div class="jumbotron text-center">
+            <h1 id="title123">
+                About Our Team
+            </h1>
+            <hr />
+            <span class="names"><a href="#brandon" class="names-link">Brandon</a>, <a href="#liana"
+                    class="names-link">Liana</a>, <a href="#ismael" class="names-link">Ismael</a>, <a href="#dane"
+                    class="names-link">Dane</a>, <a href="#linnaea" class="names-link">Linnaea</a>, <a href="#vibhav"
+                    class="names-link">Vibhav</a>, <a href="#isaac" class="names-link">Isaac</a>, <a href="#grae"
+                    class="names-link">Grae</a>, <a href="#matthew" class="names-link">Matthew</a>, <a href="#will"
+                    class="names-link">Will</a>, <a href="#linnaea" class="names-link">Linnaea</a> | <a
+                    href="mailto:contact@mechnosense.org" class="names-link">Our&nbsp;email</a></span>
+            <hr />
+        </div>
+        <!-- <h2 style="font-size: 200pt;"> -->
+        <!-- This is our about page -->
+        <!-- </h2> -->
+        <h3>Team Biographies:</h3>
+        <h4>Linnaea:</h4>
+        <p id="linnaea">
+            I'm the team lead this year and am excited to start off this year with a blast. I'm part of team rocket
+            robotics and have been part of this for about three years now. I enjoy managing our repo, making plans, and
+            coding. Outside of robotics, I like to Hike, camp, and all things outdoors. I'm a team leader and am strong
+            at long-term planning and short-term emergencies. And I really enjoy giving help whenever needed. I've
+            enjoyed these three years and am stoked to have another amazing year with team 12110 MechNoSense.
+        </p>
+        <h4>Liana:</h4>
+        <p id="liana">
+            I am the creative input for our website, a builder, and I also work on the engineering notebook/note-taking.
+            I joined in 2021 as a builder, and I worked on the robot. This year I want to learn more about building, and
+            maybe learn about coding. The website is my first big project, and I’m very excited about it.
+        </p>
+        <h4>Ismael:</h4>
+        <p id="ismael">
+            I’m Ismael and this is my first year in robotics. I am focusing on coding our robot’s teleop and doing cool
+            stuff to put in there like PID and computer vision. I am really excited to learn how to do all this. I am in
+            seventh grade and am running out of things to say but just to make it seem like mine is the longest I will
+            put this in.
+        </p>
+        <h4>Dane:</h4>
+        <p id="dane">
+            I am Dane. I am our head builder and I joined in 2021. I have loved building things all my life. I was also
+            the lead builder for last year's robotics challenge (freight frenzy). Probably the biggest reason I joined
+            was to pit my hard work against others who also love to build.
+        </p>
+        <h4>Vibhav:</h4>
+        <p id="vibhav">
+            I am Vibhav. The co-team leader, and co-lead programmer. I have been doing Robotics for 2+ years now and
+            have been programming for most of the time as well. I became interested in Robotics partly because of my
+            parents. They are both software engineers and I was very interested in their work. The other part was that I
+            had played around with coding for some time and I found that I enjoyed making small games and programs with
+            it; so that’s how I became interested in robotics and programming specifically. This year, I am very excited
+            to learn more, work harder, and do better!
+        </p>
+        <h4>Isaac:</h4>
+        <p id="isaac">
+            My name is Isaac and this is my second season of robotics in this club. I am one of our group's builders but
+            I have been working in many areas of robotics this season. I have really enjoyed robotics and I want to show
+            its greatness to anyone who wants it. My favorite color is orange, I like robotics and soccer, I'm in middle
+            school and pizza is good. Goodbyeeeee.
+        </p>
+        <h4>Grae:</h4>
+        <p id="grae">
+            Hi my name is Grae.
+        </p>
+        <h4>Matthew:</h4>
+        <p id="matthew">
+            Hi my name is Matthew.
+        </p>
+        <h4>Will:</h4>
+        <p id="will">
+            My name is Will I., programmer for team MechNoSense. I was the leader for one of the two sub-teams last
+            year: team 13749 atlas. I am currently a midline programmer for MechNoSense, as well as an assistant
+            coordinator for MechNoSense.
+        </p>
+        <h4>Brandon:</h4>
+        <p id="brandon">
+            Hi, my name is Brandon. I do the coding of this webpage. I drive the robot for the competitions.
+        </p>
+        <p id="email">
+            Our email is <a href="mailto:contact@mechnosense.org">contact@mechnosense.org</a>
+        </p>
+        <p id="YouTube">
+            Our YouTube channel is <a href="https://www.youtube.com/channel/UCBLlVrpF1RP2r3pBj7l6z8Q">MechNoSense
+                Robotics</a>
+        </p>
+    </div>
+</body>
+
 </html>
