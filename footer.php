1714d3f450c978fef4cc727b0167a08cb6b5a5b0
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-16 16:09:44 -0700

    maybe fix?

--- footer.php
+++ footer.php
@@ -1,4 +1,4 @@
-<div class="bottom-box">
+<div id="footer">
     <p>
         Phone number: <a href="tel:5410000000">(541) 000-0000</a>
         Email: <a href="mailto:contact@mechnosense.org">contact@mechnosense.org</a>
