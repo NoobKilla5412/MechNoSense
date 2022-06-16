fc8291908390a1cea4ded23685e5057c954dcf9a
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-16 16:29:08 -0700

    Update signup.php

--- signup.php
+++ signup.php
@@ -27,16 +27,19 @@ $doc = "Sign Up";
 	<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
 
 	<!-- Bootstrap CSS -->
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
+		integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
 	<!-- Bootstrap JavaScript -->
-	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+	<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"
+		integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p"
+		crossorigin="anonymous"></script>
 	<link rel="stylesheet" href="/style.css" />
 	<!-- Custom CSS -->
 	<title>Sign Up &#8211; MechNoSense</title>
 </head>
 
 <body>
-	<div class="container-fluid">
+	<div class="container-fluid" style="margin-bottom: 1rem;">
 		<!-- Navbar -->
 		<?php include '.\navbar.php'; ?>
 		<div class="jumbotron text-center">
@@ -48,20 +51,27 @@ $doc = "Sign Up";
 			<hr />
 		</div>
 		<?php if (isset($_GET['error'])) { ?>
-			<span class="error"><?= htmlspecialchars($_GET["error"]); ?></span>
+		<span class="error">
+			<?= htmlspecialchars($_GET["error"]); ?>
+		</span>
 		<?php } ?>
 		<form action="/signup_data.php" method="post">
 			<div class="input-group">
 				<label for="first" class="text">First Name</label>
-				<input type="text" id="first" name="first" class="input-group" style="background-color: #185020;" required pattern="[^,<>]*" max="20"><br />
+				<input type="text" id="first" name="first" class="input-group" style="background-color: #185020;"
+					required pattern="[^,<>]*" max="20"><br />
 				<label for="last" class="text">Last Name</label>
-				<input type="text" id="last" name="last" class="input-group" style="background-color: #185020;" required pattern="[^,<>]*" max="25"><br />
+				<input type="text" id="last" name="last" class="input-group" style="background-color: #185020;" required
+					pattern="[^,<>]*" max="25"><br />
 			</div>
 			<div class="input-group">
 				<label for="email" class="text">Enter your email:</label><br />
-				<input type="email" id="email" name="email" class="input-group" style="background-color: #185020;" required pattern="[^,<>]*" max="40"><br />
+				<input type="email" id="email" name="email" class="input-group" style="background-color: #185020;"
+					required pattern="[^,<>]*" max="40"><br />
 				<label for="phone" class="text">Enter your phone number:</label><br />
-				<input type="tel" class="input-group" id="phone" name="phone" style="background-color: #185020;" placeholder="123-456-7890" pattern="^[(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4}$" required><br /><br />
+				<input type="tel" class="input-group" id="phone" name="phone" style="background-color: #185020;"
+					placeholder="123-456-7890" pattern="^[(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4}$"
+					required><br /><br />
 			</div>
 			<br />
 			<div style="display: flex;flex-direction: column;">
@@ -70,7 +80,6 @@ $doc = "Sign Up";
 		</form>
 	</div>
 	<?php include 'C:\wamp64\www\MechNoSense\footer.php'; ?>
-	<br><br><br>
 </body>
 
 </html>
