349a162548defbaf7bd0b9c7246c7aadaa0782c6
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-16 16:23:22 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -2,5 +2,7 @@
     <p>
         Phone number: <a href="tel:5410000000">(541) 000-0000</a>
         Email: <a href="mailto:contact@mechnosense.org">contact@mechnosense.org</a>
+        YouTube channel: <a
+            href="https://www.youtube.com/channel/UCBLlVrpF1RP2r3pBj7l6z8Q">MechNoSense&nbsp;Robotics</a>
     </p>
 </div>
