ca148ae774d1bf57b984a94a44765fdd78e9d18f
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-16 16:27:08 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,11 +1,11 @@
 <div id="footer">
+    <p class="text-center">
+        <a class="btn" href="#" style="background-color: #000; color: #FFF;">^</a>
+    </p>
     <p>
         Phone number: <a href="tel:5410000000">(541) 000-0000</a>
         Email: <a href="mailto:contact@mechnosense.org">contact@mechnosense.org</a>
         YouTube channel: <a
             href="https://www.youtube.com/channel/UCBLlVrpF1RP2r3pBj7l6z8Q">MechNoSense&nbsp;Robotics</a>
-    </p>
-    <p class="text-center">
-        <a class="btn" href="#" style="background-color: #000; color: #FFF;">^</a>
     </p>
 </div>
