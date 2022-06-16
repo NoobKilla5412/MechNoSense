2ee8e64c40dc4b644954160679bbda6bb494a403
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-15 19:22:35 -0700

    Update index.php

--- workshops/index.php
+++ workshops/index.php
@@ -64,18 +64,11 @@ $doc = "Workshops";
 				// Storing data in form of JSON
 				var data = await response.json();
 				console.log(data);
-				if (response) {
-					hideloader();
-				}
 				show(data);
 			}
 			// Calling that async function
 			getapi(api_url);
 
-			// Function to hide the loader
-			function hideloader() {
-				document.getElementById('loading').style.display = 'none';
-			}
 			// Function to define innerHTML for HTML table
 			function show(data) {
 				let tab;
