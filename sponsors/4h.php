70ae976ab9e70aeaf255c0f565d355b909a96885
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-17 18:07:07 -0700

    make the navbar stay at the top

--- sponsors/4h.php
+++ sponsors/4h.php
@@ -21,6 +21,7 @@ $subdoc = "Sponsors";
 		<meta name="keywords" content="MechNoSense mechnosense robots robotics robot">
 		<link rel="icon" type="image/ico" href="/favicon.ico" />
 		<script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+	<script src="/js.js"></script>
 
 		<!-- Bootstrap CSS -->
 	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
