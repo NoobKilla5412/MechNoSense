12206e90f0117aab6332731fb8b779cfa4652be8
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-06-17 18:16:13 -0700

    fix navbar

--- minibot.php
+++ minibot.php
@@ -23,7 +23,6 @@ $doc = "Mini-Bot";
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="icon" type="image/ico" href="/favicon.ico" />
     <script src="https://ajax.GOOGLEAPIS.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-	<script src="/js.js"></script>
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet"
