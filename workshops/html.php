6c61723ac75fce55b4874ef205e04b0865f9bedc
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-07-04 15:56:51 -0700

    Update html.php

--- workshops/html.php
+++ workshops/html.php
@@ -53,10 +53,10 @@ $subdoc = "Workshops";
 &lt;!DOCTYPE html&gt;
 &lt;html lang="en"&gt;
 &lt;head&gt;
-	&lt;meta charset="UTF-8"&gt;
-	&lt;meta http-equiv="X-UA-Compatible" content="IE=edge"&gt;
-	&lt;meta name="viewport" content="width=device-width, initial-scale=1"&gt;
-	&lt;title&gt;Document&lt;/title&gt;
+    &lt;meta charset="UTF-8"&gt;
+    &lt;meta http-equiv="X-UA-Compatible" content="IE=edge"&gt;
+    &lt;meta name="viewport" content="width=device-width, initial-scale=1"&gt;
+    &lt;title&gt;Document&lt;/title&gt;
 &lt;/head&gt;
 &lt;body&gt;
 
