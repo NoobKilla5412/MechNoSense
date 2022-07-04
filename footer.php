d1242efed0332ad878af6102a386937d0399f3a3
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-07-04 15:51:25 -0700

    Update footer.php

--- footer.php
+++ footer.php
@@ -1,13 +1,12 @@
 <div id="footer">
-    <?php include 'C:\wamp64\www\MechNoSense\navbar-bottom.php';?>
+    <?php include 'C:\wamp64\www\MechNoSense\navbar-bottom.php'; ?>
     <p class="text-center" style="color: #8c8989;">
         <a class="btn" href="#" style="background-color: #000; color: #FFF;">&#9650;</a>
     </p>
     <p style="color: #8c8989;">
-        Phone number: <a href="tel:5555555555">(555) 555-5555</a>
+        Phone number: <a href="tel:5412867974">(541) 286-7974</a>
         Email: <a href="mailto:contact@mechnosense.org">contact@mechnosense.org</a>
-        YouTube channel: <a
-            href="https://www.youtube.com/channel/UCBLlVrpF1RP2r3pBj7l6z8Q">MechNoSense&nbsp;Robotics</a>
+        YouTube channel: <a href="https://www.youtube.com/channel/UCBLlVrpF1RP2r3pBj7l6z8Q">MechNoSense&nbsp;Robotics</a>
     </p>
     <br><br id="bottom">
 </div>
