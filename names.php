bedb4b62a5b769226a8bcc0467fb2697d0d683ec
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-07-04 14:24:52 -0700

    Update names.php

--- names.php
+++ names.php
@@ -25,7 +25,7 @@ if (mysqli_num_rows($result) > 0) {
         }
         ?>
         <?php
-        echo implode(', ',$names).'_';
+        echo implode(', ',$names);
         ?>
     </span>
 <?php
