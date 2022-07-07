747bc13306134afbce3840e1f2ed73ff637153c7
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-07-06 20:03:15 -0700

    Update navbar.php

--- navbar.php
+++ navbar.php
@@ -103,7 +103,7 @@ f="/robots/"><b>Robots</b></a></li>
 				</li>
 				<form class="d-flex">
 					<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
-					<button class="btn btn-outline-success" type="submit">Search</button>
+					<button class="btn btn-outline" type="submit">Search</button>
 				</form>
 				<?php /*<li class="nav-item dropdown">
 					<!-- <li class="nav-item" style="float: right;"><a class="nav-link" href="#bottom">&#9660;</a></li> -->
