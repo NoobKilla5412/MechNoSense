2eb854b588108837704be2f73ba7132897127b2c
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-10-10 19:03:54 -0700

    Uncomment the navbar-bottom.php file

--- navbar-bottom.php
+++ navbar-bottom.php
@@ -1,62 +1,3 @@
-<?php /*
-function active($doc1)
-{
-  global $doc;
-  if ($doc === $doc1) {
-    echo "<a class=\"nav-link active\" aria-current=\"page\" ";
-  } else {
-    echo "<a class=\"nav-link\" ";
-  }
-}
-function activeDrop($doc1)
-{
-  global $doc;
-  global $subdoc;
-  if ($doc === $doc1 || $subdoc === $doc1) {
-    echo "active";
-  } else {
-    echo "";
-  }
-}
-function activeSubDrop($doc1)
-{
-  global $doc;
-  global $subdoc;
-  if ($doc === $doc1) {
-    echo "active";
-  } else {
-    echo "";
-  }
-}*/
-?>
-<?php /*
-href="/buy/">Buy</a></li>
-');?>href="/pics/">Pictures</a></li>
-;?>href="/videos/">Videos</a></li>
-);?>href="/signup/">Sign Up</a>
-g');?>href="/daily-log/">Daily Log</a>
-s');?>href="/workshops/">Workshops</a>
-');?>href="/about-us/">About Us</a></li>
->href="/">Home</a></li>
-########################################################
-href="/sponsors/"><b>Sponsors</b></a></li>
-<a href="/sponsors/hp">HP</a></li>
-<a href="/sponsors/4h">4H</a></li>
-;?><a href="/sponsors/first">First Robotics</a></li>
-########################################################
-
-########################################################
-f="/robots/"><b>Robots</b></a></li>
-><a href="/robots/titans#titans">Titans</a></li>
-<a href="/robots/atlas#atlas">Atlas</a></li>
-########################################################
-
-########################################################
- href="/buy"><b>Buy</b></a></li>
-');?><a href="/buy#titans">Titans</a></li>
-);?><a href="/buy#atlas">Atlas</a></li>
-########################################################
-*/ ?>
 <nav class="navbar navbar-expand-lg navbar-dark" style="background-color: #0000;">
   <div class="container-fluid">
     <a class="navbar-brand" href="/">MechNoSense</a>
@@ -101,17 +42,6 @@ f="/robots/"><b>Robots</b></a></li>
             <li><a class="dropdown-item <?php activeSubDrop("Robots-Atlas"); ?>" href="/robots/atlas">Atlas</a></li>
           </ul>
         </li>
-        <?php /*<li class="nav-item dropdown">
-          <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-            Dropdown
-          </a>
-          <ul class="dropdown-menu" aria-labelledby="navbarDropdown" style="background-color: #183820;">
-            <li><a class="dropdown-item" href="#">Action</a></li>
-            <li><a class="dropdown-item" href="#">Another action</a></li>
-            <li><hr class="dropdown-divider"></li>
-            <li><a class="dropdown-item" href="#">Something else here</a></li>
-          </ul>
-        </li>*/ ?>
       </ul>
     </div>
   </div>
