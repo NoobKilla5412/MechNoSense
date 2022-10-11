f8ed742d3e703d40118285455cb1184d62f62b74
NoobKilla5412 <92069057+NoobKilla5412@users.noreply.github.com>
2022-10-10 19:06:33 -0700

    Change signup_data.php

--- signup_data.php
+++ signup_data.php
@@ -1,7 +1,5 @@
 <?php
 
-
-
 function checkPhone($pattern, $number)
 {
   if (preg_match($pattern, $number) === 1) {
@@ -15,19 +13,12 @@ $phone_pattern = "/^[(]*([0-9]{3})[- .)]*[0-9]{3}[- .]*[0-9]{4}$/";
 $path = "../data.csv";
 if (!empty($_POST["first"]) && !empty($_POST["last"]) && !empty($_POST["email"]) && !empty($_POST["phone"]) && checkPhone($phone_pattern, $_POST['phone'])) {
 
-
   $length_name = strlen($_POST["first"]);
-
 
   if ($length_name > 2 && $length_name < 100) {
     $first = $_POST["first"];
   }
-
-
-
-
-
-
+  
   $fh = fopen($path, "a+");
   $string = "\n\"" . strip_tags($first) . "\",\"" . strip_tags($_POST["last"]) . "\",\"" . strip_tags(filter_var($_POST["email"], FILTER_SANITIZE_EMAIL)) . "\",\"" . strip_tags($_POST['phone']) . "\"";
   fwrite($fh, $string); // Write information to the file
